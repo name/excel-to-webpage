ab081cad7c673f5e5082079ba87d3842b37e31a7
charlie <charlie@maddex.co>
2020-06-22 20:16:44 +0100

    used xlsx to read spreadsheet and output to excel page

--- application/app.js
+++ application/app.js
@@ -7,4 +7,49 @@ app.get('/', (req, res) => {
     res.render('index')
 });
 
+app.get('/excel', (req, res) => {
+    var XLSX = require('xlsx');
+    var workbook = XLSX.readFile('excel/example.xlsx');
+    var sheet_name_list = workbook.SheetNames;
+    var output = '';
+    sheet_name_list.forEach(function(y) {
+        var worksheet = workbook.Sheets[y];
+        var headers = {};
+        var data = [];
+        for(z in worksheet) {
+            if(z[0] === '!') continue;
+            //parse out the column, row, and value
+            var tt = 0;
+            for (var i = 0; i < z.length; i++) {
+                if (!isNaN(z[i])) {
+                    tt = i;
+                    break;
+                }
+            };
+            var col = z.substring(0,tt);
+            var row = parseInt(z.substring(tt));
+            var value = worksheet[z].v;
+
+            //store header names
+            if(row == 1 && value) {
+                headers[col] = value;
+                continue;
+            }
+
+            if(!data[row]) data[row]={};
+            data[row][headers[col]] = value;
+        }
+        //drop those first two rows which are empty
+        data.shift();
+        data.shift();
+        console.log(data);
+        output = data;
+    });
+    res.render('excel', { output : output })
+});
+
+app.get('/settings', (req, res) => {
+    res.render('settings')
+});
+
 app.listen(port, console.log(`Server listening at http://localhost:${port}`));
