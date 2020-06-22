32a75e02905bac809f38bdd3dc144f15a20dd6ff
charlie <charlie@maddex.co>
2020-06-22 19:48:53 +0100

    changed view engine to ejs and rendered index

--- application/app.js
+++ application/app.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const port = 4000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('root');
+    res.render('index')
 });
 
 app.listen(port, console.log(`Server listening at http://localhost:${port}`));
